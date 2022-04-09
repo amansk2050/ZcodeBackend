e1ef46cc4b41149b2250e7fe0bca538b425995a1
rapidAman <sheikhaman@rapidinnovation.dev>
2022-04-09 12:51:44 +0530

    tried ftp but removed

---
+++ README.md
@@ -0,0 +1,6 @@
+var connection = mysql.createConnection({
+  host: "zcode.dog",
+  user: "zcodedog_db_user",
+  password: "%XjCy]SZCzLQ",
+  database: "zcodedog_data",
+});
--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const util = require("util");
 const express = require("express");
 const bodyParser = require("body-parser");
 var app = express();
-var cors = require('cors');
+var cors = require("cors");
 app.use(cors());
 app.use(bodyParser.json());
 
 // MYSQL details
-
+console.log("hello aman");
 // var connection = mysql.createConnection({
 //   host: "localhost",
 //   user: "root",
@@ -18,24 +18,26 @@ app.use(bodyParser.json());
 
 //-- for production --
 var connection = mysql.createConnection({
-  host: "zcode.dog",
+  host: "localhost",
   user: "zcodedog_db_user",
   password: "%XjCy]SZCzLQ",
   database: "zcodedog_data",
 });
+
+
 const query = util.promisify(connection.query).bind(connection);
 
 // get wallet address by code // with query params
 // request url - http://localhost:3000/getdata
 app.get("/getdata", async (req, res) => {
   try {
-    console.log("hello :: ",req.query.code);
+    console.log("hello :: ", req.query.code);
     const code = await query(
       `select address from affiliate where id = '${req.query.code}';`
     );
     var data = JSON.stringify(code);
     var data1 = JSON.parse(data);
-    console.log("data ! ",data1)
+    console.log("data ! ", data1);
     res.status(200).json({
       message: "Address found",
       address: data1[0].address,
@@ -49,9 +51,9 @@ app.get("/getdata", async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("app is runnig on ",process.env.PORT );
-});
+// app.listen(3000, function () {
+//   console.log("app is runnig");
+// });
 
 /* 
 
@@ -83,7 +85,7 @@ app.post("/generate", async (req, res) => {
       );
       var data2 = JSON.stringify(getId);
       var data3 = JSON.parse(data2);
-      console.log("code :: ",data3);
+      console.log("code :: ", data3);
       res.status(200).json({
         message: "Code generated",
         code: `${data3[0].id}ZCODE`,
@@ -125,13 +127,15 @@ app.get("/getTop", async (req, res) => {
   try {
     console.log("hello");
     const code = await query(
-      `select *  from affiliate offset order by nft_minted desc limit 10 offset ${(req.query.page - 1) * 10};`
+      `select *  from affiliate offset order by nft_minted desc limit 10 offset ${
+        (req.query.page - 1) * 10
+      };`
     );
     var data = JSON.stringify(code);
     var data1 = JSON.parse(data);
     res.status(200).json({
       count: data1.length,
-      code: data1
+      code: data1,
     });
   } catch (error) {
     console.log("error :: ", error);
